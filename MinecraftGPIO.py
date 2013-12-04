13255ed490e647a2bc914adeb124b852fb095d72
Stonie <dev@null.com>
2013-12-04 23:22:32 +1100

    added minecraft api to keep it simple

--- MinecraftGPIO.py
+++ MinecraftGPIO.py
@@ -6,30 +6,31 @@ import block
 import time
 
 # The pin number used for input
-pin = 22
+pin = 18
 
-# set the pin number mode
+# set the pin mode and default state
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# set our pin to input mode
-GPIO.setup(pin, GPIO.IN)
-
+# connect to the local Minecraft server
 mc = minecraft.Minecraft.create()
-mc.postToChat("Hello From the Minecraft API")
-mc.setBlock(0, 10, 0, block.STONE)
+mc.postToChat("Hello from the  Minecraft API :)")
+time.sleep(10)
+mc.postToChat('Press your hardware button for some new flooring')
 
-buttonStateHasChanged = 0
-
+# control loop
 while True:
-  #take a reading
-  input = GPIO.input(pin)
-  #if the last reading was low and this one high, print
-  if ((not buttonStateHasChanged) and input):
-    print("Button pressed")
-  #update previous input
-  prev_input = input
-  #slight pause to debounce
-  time.sleep(0.05)
-
+	
+	buttonInput = GPIO.input(pin)
+  	if buttonInput == GPIO.LOW:
+		#print('input low')
+		time.sleep(0.05)
+  	else:
+		print('button pressed!')
+                # find the players location in minecraft
+		playerTilePos = mc.player.getTilePos()
+		# create a dimond pad 10 x 10 under the player
+		mc.setBlocks(playerTilePos.x - 5, playerTilePos.y - 1, playerTilePos.z - 5, playerTilePos.x + 5, playerTilePos.y -1, playerTilePos.z + 5, block.DIAMOND_BLOCK)
+                time.sleep(0.20) # really rough button debounce
 
 
